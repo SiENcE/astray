47a0572e606f0fdebdcd5b75eb7ad70cbf07fb13
SiENcE <seabeams@gmx.de>
2015-09-28 23:00:18 +0200

    -sample added

--- main.lua
+++ main.lua
@@ -19,9 +19,9 @@ function love.load()
 	print('Astay Sample')
 	local symbols = {Wall='#', Empty=' ', DoorN='-', DoorS='-', DoorE='|', DoorW='|'}
 
-	--	local generator = astray.Astray:new( 25, 25, 30, 70, 80, astray.RoomGenerator:new(8, 3, 6, 3, 6) )
+	local generator = astray.Astray:new( 25, 25, 30, 70, 80, astray.RoomGenerator:new(8, 3, 6, 3, 6) )
 --	local generator = astray.Astray:new( 5, 5, 1, 15, 5, astray.RoomGenerator:new(1, 1, 2, 1, 2) )
-	local generator = astray.Astray:new( 20, 10, 15, 70, 80, astray.RoomGenerator:new(4, 2, 6, 2, 6) )
+--	local generator = astray.Astray:new( 20, 10, 15, 70, 80, astray.RoomGenerator:new(4, 2, 6, 2, 6) )
 --	local dungeon = generator:Generate()
 --	local tiles = generator:CellToTiles(dungeon, symbols )
 --	print("Mazesize=", #tiles+1, #tiles[1]+1 )
