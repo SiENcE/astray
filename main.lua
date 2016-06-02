ce41c034aaaa7aedfabdcf99b347bded20fe6c87
Florian <seabeams@gmx.de>
2016-06-02 14:24:34 +0200

    modified sample dungeon generation

--- main.lua
+++ main.lua
@@ -16,17 +16,26 @@ function drawdungeon(tiles, width, height)
 end
 
 function love.load()
-	print('Astay Sample')
+	print('Astay Sample\n')
 	local symbols = {Wall='#', Empty=' ', DoorN='-', DoorS='-', DoorE='|', DoorW='|'}
 
-	local generator = astray.Astray:new( 25, 25, 30, 70, 80, astray.RoomGenerator:new(8, 3, 6, 3, 6) )
+	print('Automatic\n------------------------------------------\n')
+	-- original setup
+	local generator = astray.Astray:new( 25, 25, 30, 70, 50, astray.RoomGenerator:new(10,1,5,1,5) )
+	local dungeon = generator:Generate()
+	local tiles = generator:CellToTiles(dungeon, symbols )
+	print("Mazesize=", #tiles+1, #tiles[1]+1 )
+	drawdungeon(tiles, #tiles, #tiles[1] )
+
+--[[
+	print('\n-----------------------------------------------------\n')
+	-- or manually each step
+	print('Manually\n------------------------------------------\n')
+
+--	local generator = astray.Astray:new( 25, 25, 30, 70, 80, astray.RoomGenerator:new(8, 3, 6, 3, 6) )
 --	local generator = astray.Astray:new( 5, 5, 1, 15, 5, astray.RoomGenerator:new(1, 1, 2, 1, 2) )
---	local generator = astray.Astray:new( 20, 10, 15, 70, 80, astray.RoomGenerator:new(4, 2, 6, 2, 6) )
---	local dungeon = generator:Generate()
---	local tiles = generator:CellToTiles(dungeon, symbols )
---	print("Mazesize=", #tiles+1, #tiles[1]+1 )
---	drawdungeon(tiles, #tiles, #tiles[1] )
-	
+	local generator = astray.Astray:new( 20, 10, 15, 70, 80, astray.RoomGenerator:new(4, 2, 6, 2, 6) )
+
 	local dungeon = generator:GenerateDungeon()
 	local tiles = generator:CellToTiles(dungeon, symbols )
 	drawdungeon(tiles, #tiles, #tiles[1] )
@@ -42,6 +51,7 @@ function love.load()
 	generator:GeneratePlaceDoors(dungeon)
 	local tiles = generator:CellToTiles(dungeon, symbols )
 	drawdungeon(tiles, #tiles, #tiles[1] )
+]]--
 end
 
 function love.draw()
